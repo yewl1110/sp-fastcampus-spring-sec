0bd4bb4794ffae147d292d8eb386b84734efb592
신예지 <yewl1110@naver.com>
2022-07-01 11:18:01 +0900

    gradle 프로젝트 초기설정

---
+++ server/basic-test/src/test/java/com/sp/fc/Test1.java
@@ -0,0 +1,14 @@
+package com.sp.fc;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Test1 {
+
+    @DisplayName("")
+    @Test
+    void test() {
+        assertEquals("test", "test");
+    }
+}
